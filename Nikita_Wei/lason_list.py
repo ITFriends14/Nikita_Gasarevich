946c05a891596eb6604f66c5e4fd2e5a73d5d723
ITFriends14 <128975094+ITFriends14@users.noreply.github.com>
2023-04-02 19:12:32 +0300

    Add files via upload

---
+++ Nikita_Wei/lason_list.py
@@ -0,0 +1,31 @@
+names = set()
+
+print(names)
+names.add(" Roman ")
+
+devises1 = ( 1 ,2, 3, 4, 5,)
+
+
+
+
+
+
+
+
+
+
+
+
+nikita = set()
+nikita.add ( 1346)
+
+
+
+
+
+
+
+
+
+
+
---
+++ Nikita_Wei/leson7.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+turtle.color('purple')
+
+
+# turtle right 
+# forward
+
+turtle.right(90)
+turtle.forward(100)
+
+turtle.right(90)
+turtle.forward(100)
+
+turtle.color("red","orange")
+
+for i in range(4):
+    turtle.right(90)
+    turtle.forward(100)
